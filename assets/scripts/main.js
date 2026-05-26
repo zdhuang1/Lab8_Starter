ea4ca392ae92ca7d5882211a834ddd83e5435d16
Zachary Huang <zdhuang@ucsd.edu>
2026-05-26 16:02:15 -0700

    add service worker

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -33,27 +33,18 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
-  // EXPLORE - START (All explore numbers start with B)
-  /*******************/
-  // ServiceWorkers have many uses, the most common of which is to manage
-  // local caches, intercept network requests, and conditionally serve from
-  // those local caches. This increases performance since users aren't
-  // re-downloading the same resources every single page visit. This also allows
-  // websites to have some (if not all) functionality offline! I highly
-  // recommend reading up on ServiceWorkers on MDN before continuing.
-  /*******************/
-  // We first must register our ServiceWorker here before any of the code in
-  // sw.js is executed.
-  // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  // B1. Check if serviceWorker is supported
+  if ('serviceWorker' in navigator) {
+    // B2. Listen for the load event
+    window.addEventListener('load', () => {
+      // B3. Register the service worker
+      navigator.serviceWorker.register('./sw.js')
+        // B4. Log success
+        .then((reg) => console.log('Service worker registered!', reg))
+        // B5. Log failure
+        .catch((err) => console.log('Service worker failed to register', err));
+    });
+  }
 }
 
 /**
